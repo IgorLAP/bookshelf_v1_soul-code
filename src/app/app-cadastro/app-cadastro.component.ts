52fc6c3eae478b795ca7d8c5f710af36920e395d
Icaro Ferreira <icaro.ferreira.filho@gmail.com>
2022-01-27 17:13:02 -0300

    repaginação de cadastro

--- src/app/app-cadastro/app-cadastro.component.ts
+++ src/app/app-cadastro/app-cadastro.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 import { AutenticacaoFirebaseService } from './../servicosInterface/autenticacao-firebase.service';
 
@@ -12,67 +21,103 @@ export function passwordMatchValidator(): ValidatorFn {
 
     if (senha && confirma && senha !== confirma) {
       return {
-        senhaConfirmada: true
-      }
+        senhaConfirmada: true,
+      };
     }
     return null;
-  }
+  };
 }
 
 @Component({
   selector: 'app-app-cadastro',
   templateUrl: './app-cadastro.component.html',
-  styleUrls: ['./app-cadastro.component.scss']
+  styleUrls: ['./app-cadastro.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
 })
 export class AppCadastroComponent implements OnInit {
-
-  formularioCadastro = this.loginBuilder.group({
-    nome: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    senha: new FormControl('', Validators.required),
-    confirmaSenha: new FormControl('', Validators.required),
-  }, { validators: passwordMatchValidator() });
-
+/*   formularioCadastro = this.loginBuilder.group(
+    {
+      nome: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', Validators.required),
+      confirmaSenha: new FormControl('', Validators.required),
+    },
+    { validators: passwordMatchValidator() }
+  ); */
+  clientName!: string;
+  clientEmail!: string;
+  clienteSenha!: string;
+  clienteConfirmSenha!:string
+  isLinear = false;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
   constructor(
     private loginBuilder: FormBuilder,
     private autenticacaoFirebaseService: AutenticacaoFirebaseService,
     private toast: HotToastService,
     private rotas: Router
-  ) { }
+  ) {}
 
-  get nome() {
-    return this.formularioCadastro.get('nome')
+/*   get nome() {
+    return this.formularioCadastro.get('nome');
   }
 
   get email() {
-    return this.formularioCadastro.get('email')
+    return this.formularioCadastro.get('email');
   }
 
   get senha() {
-    return this.formularioCadastro.get('senha')
+    return this.formularioCadastro.get('senha');
   }
 
   get confirmaSenha() {
-    return this.formularioCadastro.get('confirmaSenha')
-  }
+    return this.formularioCadastro.get('confirmaSenha');
+  } */
 
+  mostrar() {
+    console.log('Nome => ', this.clientName);
+    console.log('Email => ', this.clientEmail);
+    console.log('Senha => ', this.clienteSenha);
+    console.log('Confirma Senha => ', this.clienteConfirmSenha);
+  }
   enviaCadastro() {
-    if (!this.formularioCadastro.valid) {
-      return;
+    if(!this.clientName || !this.clientEmail || !this.clienteSenha || !this.clienteConfirmSenha){
+      this.toast.error('Preencha todos os campos corretamente!')
+      return
+    } else if(this.clienteSenha !== this.clienteConfirmSenha){
+      this.toast.error('As senhas não conferem!')
+      return
     }
-    const { nome, email, senha } = this.formularioCadastro.value;
+
+    /* if (!this.formularioCadastro.valid) {
+      console.log('nao foi')
+      return;
+    } */
+    // const { nome, email, senha } = this.formularioCadastro.value;
     this.autenticacaoFirebaseService
-      .cadastrarUsuario(nome, email, senha)
+      .cadastrarUsuario(this.clientName, this.clientEmail, this.clienteSenha)
       .pipe(
         this.toast.observe({
           success: 'Cadatro executado, bem vindo ao BookShelf',
           loading: 'Enviando informações...',
           error: ({ message }) => `Houve um problema: #BS${message}`,
         })
-      ).subscribe(() => {
-        this.rotas.navigate(['/'])
+      )
+      .subscribe(() => {
+        this.rotas.navigate(['/']);
       });
   }
-  ngOnInit(): void {
+  ngOnInit() {
+    this.firstFormGroup = this.loginBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this.loginBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
   }
 }
--- src/app/app-compartilhado/app-compartilhado.module.ts
+++ src/app/app-compartilhado/app-compartilhado.module.ts
@@ -4,19 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppDialogosComponent } from './app-dialogos/app-dialogos.component';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { PipesPipepe } from './app-pipes/pipes.pipe';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
-  declarations: [
-    AppDialogosComponent,
-    PipesPipepe
-  ],
-  imports: [
-    CommonModule,
-    AppMaterialModule
-  ],
-  exports: [
-    AppDialogosComponent,
-    PipesPipepe
-  ]
+  declarations: [AppDialogosComponent, PipesPipepe],
+  imports: [CommonModule, AppMaterialModule],
+  exports: [AppDialogosComponent, PipesPipepe, MatStepperModule],
 })
-export class AppCompartilhadoModule { }
+export class AppCompartilhadoModule {}
