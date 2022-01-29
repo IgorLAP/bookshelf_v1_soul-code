cbece6fac74d33fe6eb7ca2a95052377d57fa4d7
Igor Pedrosa <ilap1036@gmail.com>
2022-01-29 10:04:48 -0300

    component de susgestoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { FeedComponent } from './feed/feed.component';
 import { IsbnComponent } from './isbn/isbn.component';
 import { RecuperarSenhaComponent } from './recuperar-senha/recuperar-senha.component';
 import { RotasDesconhecidasComponent } from './rotas-desconhecidas/rotas-desconhecidas.component';
+import { SugestoesComponent } from './sugestoes/sugestoes.component';
 
 
 const enviarSemLogin = () => redirectUnauthorizedTo(['/app-app-cadastro']);
@@ -38,6 +39,10 @@ const routes: Routes = [
   },
   {
     path: 'isbn', component: IsbnComponent,
+    ...canActivate(enviarSemLogin)
+  },
+  {
+    path: 'sugestoes', component: SugestoesComponent,
     ...canActivate(enviarSemLogin)
   },
   {
