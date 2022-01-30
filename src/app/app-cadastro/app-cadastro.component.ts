7d803503a4154cfe485b2cef9bb5a11549fe0698
Igor Pedrosa <ilap1036@gmail.com>
2022-01-30 15:36:09 -0300

    melhorias de ui/ux dashboard, cdd, usuarioMenu e ajustes json

--- src/app/app-cadastro/app-cadastro.component.ts
+++ src/app/app-cadastro/app-cadastro.component.ts
@@ -1,3 +1,4 @@
+import { catchError, of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import {
   AbstractControl,
@@ -65,6 +66,9 @@ export class AppCadastroComponent implements OnInit {
     this.autenticacaoFirebaseService
       .cadastrarUsuario(this.clientName, this.clientEmail, this.clienteSenha)
       .pipe(
+        catchError(error => {
+          return of([])
+        }),
         this.toast.observe({
           success: 'Cadatro executado, bem vindo ao BookShelf',
           loading: 'Enviando informações...',
